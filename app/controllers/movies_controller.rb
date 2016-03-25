8ca239840cc6aecd24195d966464f82206050712
Ali Certel <alicertel@hotmail.com>
2016-03-25 18:41:56 +0200

    added create user movie action endpoint

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,31 @@
 class MoviesController < ApplicationController
+	protect_from_forgery with: :null_session
+	before_action :authenticate, except: [:show, :list]
+
 	def list
 		@movies = Movie.all
 		render json: @movies
 	end
 
 	def show
-		movie = Movie.find(@movie.id)
-		render json: @movie
+		movie = Movie.find(params["id"])
+		render json: movie
 	end
 
+
+	def create_action
+		movie = Movie.find(params["id"])
+		user_movie_action = UserMovieAction.first_or_create(user: @authenticated_user, movie: movie)
+		user_movie_action.action_type=params[:action_type]
+		user_movie_action.save
+		render json: user_movie_action
+	end
+
+
+	protected
+	def authenticate
+		authenticate_or_request_with_http_token do |token, options|
+		  @authenticated_user=User.find_by(token: token)
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'movies', to: 'movies#list'
   get '/movies/:id', to: 'movies#show', as: 'movie'
+  post '/movies/:id/actions/', to: 'movies#create_action', as: 'actioned_movie'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
